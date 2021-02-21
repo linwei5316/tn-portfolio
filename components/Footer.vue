0dd7f5dcca11fb8316f9341408d8cbc38c4fcebe
Leo <linwei5316@gmail.com>
2021-02-21 23:35:53 +0800

    chore: footer link list

--- components/Footer.vue
+++ components/Footer.vue
@@ -3,11 +3,15 @@
     <div class="container text-center">
       <h5 class="text-white">TING EN WEI</h5>
       <h5 class="text-white">nn121393@gmail.com</h5>
-      <div class="flex justify-center">
-        <Button>
-
-        </Button>
-<!--        //TODO link list-->
+      <div class="mt-3 flex justify-center">
+        <a
+          class="linkListItem"
+          v-for="item in linkList"
+          :key="item.link"
+          :href="item.link"
+        >
+          <img class="w-6 h-6" :src="item.image">
+        </a>
       </div>
     </div>
   </footer>
@@ -18,9 +22,31 @@ import Vue from 'vue'
 
 export default Vue.extend({
   name: "Footer",
+  data() {
+    return {
+      linkList: [
+        {
+          image: require('@/assets/images/footer-medium.svg'),
+          link: '',
+        },
+        {
+          image: require('@/assets/images/footer-behance.svg'),
+          link: '',
+        },
+        {
+          image: require('@/assets/images/footer-linkedin.svg'),
+          link: '',
+        },
+      ]
+    }
+  }
 })
 </script>
 
-<style scoped>
-
+<style lang="scss" scoped>
+.linkListItem {
+  &:not(:last-of-type) {
+    @apply mr-1.5;
+  }
+}
 </style>
